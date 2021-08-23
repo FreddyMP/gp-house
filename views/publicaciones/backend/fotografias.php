642d1f96d41fc43063f89a032395d0dd641bf0b8
FreddyMP <freddypimpns@gmail.com>
2021-08-22 23:18:29 -0400

    fotos

--- views/publicaciones/backend/fotografias.php
+++ views/publicaciones/backend/fotografias.php
@@ -4,13 +4,8 @@
         function Fotos(){
             $this->ruta= "../../../backend/cone.php";
         }
-        function insert_picture($foto, $temporal){
-                $directorio = '../../../asset/img_publicaciones/';
-                $subir= $directorio.basename($foto);
-                move_uploaded_file($temporal, $subir);
-                $foto2 = $foto;
-                echo $foto2;
-    
+        function insert_picture($foto){
+            
         }
 
     }
